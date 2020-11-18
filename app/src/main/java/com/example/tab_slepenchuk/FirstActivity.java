6e8b0e156b9b027877a0f7ad66f1bf0a14f8b7f4
Slepenchuk <74017330+Slepenchuk@users.noreply.github.com>
2020-11-18 09:40:51 +0300

    Tab_Slepenchuk

---
+++ app/src/main/java/com/example/tab_slepenchuk/FirstActivity.java
@@ -0,0 +1,24 @@
+package com.example.tab_slepenchuk;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.NotNull;
+
+public class FirstActivity extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(@NotNull LayoutInflater inflater, @NonNull ViewGroup container,
+                             @Nullable Bundle sevedInstanseState)
+    {
+        return inflater.inflate(R.layout.first_main, container, false);
+    }
+
+}
